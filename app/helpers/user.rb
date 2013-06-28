6a3fd031b2e593e64fb11e715952e9add7e2b7e7
Apprentice <apprentice@dbc09.local>
2013-06-28 12:00:43 -0700

    Added signup and sign in forms for the home page, edited routing and implemented password encryption into the database.

--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -1,23 +1,23 @@
 helpers do
 
-def current_user
-  if session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
-end
 
-def logged_in?
-  !current_user.nil?
-end
+  def logged_in?
+    !current_user.nil?
+  end
 
-def set_current_user(user_email)
-  User.find_by_email(user_email).id
-end
+  def set_session_id(user_email)
+    session[:user_id] = User.find_by_email(user_email).id
+  end
 
-def create_user(info)
-  info[:salt] = SecureRandom.base64(6)
-  info[:password_hash] = Digest::MD5.hexdigest(info[:password_hash] + info[:salt])
-  info
-end
+  def parse_user_info(info)
+    info[:salt] = SecureRandom.base64(6)
+    info[:password_hash] = Digest::MD5.hexdigest(info[:password_hash] + info[:salt])
+    info
+  end
 
 end

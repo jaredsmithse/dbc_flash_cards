2b2ecc7051de00ae8a5e37c6dfc4fdf2f3ef06ff
Apprentice <apprentice@dbc09.local>
2013-06-28 17:53:19 -0700

    entire sprint in 45 minutes bitches.  implemented basic gameplay.

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,6 +1,11 @@
 get '/deck/:deck_id' do
 
   @deck = Deck.find(params[:deck_id])
-  @cards = @deck.cards
+  cards = @deck.cards
+  @card = cards.shuffle!.pop
   erb :card
+end
+
+post '/guess' do
+  params[:current_guess] == @card.word
 end
